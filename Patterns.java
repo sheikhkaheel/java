708a2b66a76e5538eabe278350a5b625f7272d83
sheikhkaheel <sheikhkaheel@gmail.com>
2023-10-08 21:02:00 +0530

    added traingleArea.java

--- Patterns.java
+++ Patterns.java
@@ -52,5 +52,17 @@ public class Patterns {
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        for(int i = 4; i >= 1; i--){
+            for(int j = 1; j <= 4-i; j++){
+                System.out.print("  ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
