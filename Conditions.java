46e6a3fb871d9b4641b85e0573640b65f800a47d
sheikhkaheel <sheikhkaheel@gmail.com>
2023-10-24 11:14:44 +0530

    java programes

--- Conditions.java
+++ Conditions.java
@@ -2,13 +2,14 @@ import java.util.*;
 public class Conditions {
     public static void main(String[] args){
         System.out.println("Enter age");
-        Scanner sc = new Scanner(System.in);     // Scanner is used to take input from user
-        int age = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int age = sc.nextInt();
 
-        if(age < 18){
-            System.out.println("You are minor");
-        } else{
-            System.out.println("Your are adult");
+            if(age < 18){
+                System.out.println("You are minor");
+            } else{
+                System.out.println("Your are adult");
+            }
         }
     }
 } 
--- Patterns.java
+++ Patterns.java
@@ -3,7 +3,7 @@ public class Patterns {
 
         System.out.println();
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 5; i++){
             for(int j = 0; j < 5; j++){
                 System.out.print("* ");
             }
@@ -12,9 +12,9 @@ public class Patterns {
 
         System.out.print("\n");
 
-        for(int i = 1; i <=4; i++){
-            for(int j = 1; j <=5; j++){
-                if(i == 1 || j == 1 || i == 4 || j == 5){
+        for(int i = 0; i < 5; i++){
+            for(int j = 1; j <= 5; j++){
+                if(i == 0 || j == 1 || i == 4 || j == 5){
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
