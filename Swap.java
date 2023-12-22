fb74077e98c7dd0469ae77bb34f601741234ed69
sheikhkaheel <sheikhkaheel@gmail.com>
2023-12-22 21:23:08 +0530

    added swap.java

---
+++ Swap.java
@@ -0,0 +1,14 @@
+public class Swap {
+    public void printSwap(int num1,int num2){
+        int temp = num1;
+        int a = num2;
+        int b = temp;
+
+        System.out.println("Swap of num1 + num2 " + a + " " + b);
+    }
+
+    public static void main(String[] args){
+        Swap sw = new Swap();
+        sw.printSwap(2,4);
+    }
+}
